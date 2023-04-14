c67470d29b283663ba7a11b76268309e0557b548
Bruno da Anunciação dos Santos <bruno.danuncia@gmail.com>
2023-04-14 17:42:43 -0300

    adding files

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,33 @@
+import Banner from "components/Banner/index.js";
+import Card from "components/Card/index.js";
+import Titulo from "components/Titulo/index.js";
+import styles from "./Inicio.module.css";
+import { useEffect, useState } from "react";
+
+function Inicio(){
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch("https://my-json-server.typicode.com/brunodaanunciacao/cinetag-api/videos")
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideos(dados)
+        })
+    }, [])
+
+    return (
+        <>
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus videos e filmes</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card { ...video} key={video.id} />
+                })}
+            </section>
+        </>
+    )
+}
+
+export default Inicio;
